bf35bfe7ccac371ff720e99f4b9710c7daf0bf3c
samjlevy <sjl273@cornell.edu>
2017-01-26 11:29:14 -0500

    adding times to parse

---
+++ ConditionalExcellParseout.m
@@ -0,0 +1,10 @@
+function ConditionalExcellParseout( )
+%loads one of our DNMP/ForcedUnforced excell sheets and parses out frame
+%identity by looking at txt for column names; column names have to meet
+%hardcoded labels
+%loads multiple spreadsheets into a struct(spreadsheetNum).frames, .txt,
+%loops through that struct to get locations of desired columns
+%might also be used to spit out conditional frame parsing based on what
+%kind of names we find (e.g., do we have start delay' or not?)
+
+end
